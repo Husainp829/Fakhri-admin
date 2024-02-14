0a2c1f38a315637012a71108a4689f7e80a14b14
Husain Poonawala <husain.poonawala@alerzo.com>
2024-02-14 19:59:57 +0530

    remove unnecessary button

--- src/containers/admin/list.js
+++ src/containers/admin/list.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { List, Datagrid, TextField, EmailField } from "react-admin";
-import ReceiptUpload from "../../components/uploadHelpers/ReceiptUpload";
 
 export default (props) => (
-  <>
-  <ReceiptUpload />
-    <List {...props}>
-      <Datagrid rowClick="edit">
-        <TextField source="name" />
-        <EmailField source="email" />
-      </Datagrid>
-    </List>
-  </>
+  <List {...props}>
+    <Datagrid rowClick="edit">
+      <TextField source="name" />
+      <EmailField source="email" />
+    </Datagrid>
+  </List>
 );
