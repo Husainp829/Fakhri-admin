669fa4fc7c64d9cee7a1e44f97bcb39797b1e32e
Husain Poonawala <husain.poonawala@alerzo.com>
2024-02-26 10:38:32 +0530

    updated success message of niyaaz

--- src/containers/niyaaz/create/index.js
+++ src/containers/niyaaz/create/index.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-console */
 import React, { useContext } from "react";
-import { Create, useNotify, SimpleForm } from "react-admin";
+import { Create, useNotify, SimpleForm, useRedirect } from "react-admin";
 import NiyaazForm from "./niyaazForm";
 import { EventContext } from "../../../dataprovider/eventProvider";
 import { calcTotalPayable } from "../../../utils";
 
 export default (props) => {
   const notify = useNotify();
+  const redirect = useRedirect();
   const onFailure = (error) => {
     notify(`Could not edit post: ${error.message}`);
   };
@@ -54,8 +55,13 @@ export default (props) => {
     }
     return errors;
   };
+
+  const onSuccess = (data) => {
+    notify(`Niyaaz form created - ${data.id}`, { autoHideDuration: 5000, type: "success" });
+    redirect("/niyaaz");
+  };
   return (
-    <Create {...props} onFailure={onFailure} redirect="list">
+    <Create {...props} onFailure={onFailure} redirect="list" mutationOptions={{ onSuccess }}>
       <SimpleForm warnWhenUnsavedChanges validate={validateNiyaazCreation}>
         <NiyaazForm />
       </SimpleForm>
