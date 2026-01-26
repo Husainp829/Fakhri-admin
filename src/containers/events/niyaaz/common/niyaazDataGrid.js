e4b323923febb6562391af6d7c7afd3e82a234b9
Husain Poonawala <husain.poonawala@alerzo.com>
2026-01-26 21:59:30 +0530

    added late payment

--- src/containers/events/niyaaz/common/niyaazDataGrid.js
+++ src/containers/events/niyaaz/common/niyaazDataGrid.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Datagrid, TextField, NumberField, FunctionField } from "react-admin";
+import { Datagrid, TextField, NumberField, FunctionField, BooleanField } from "react-admin";
 
 export default () => (
   <Datagrid bulkActionButtons={false}>
     <FunctionField
       label="Event Name"
       source="event"
-      render={(record) => <span>{record?.event?.name || "-"}</span>}
+      render={(record) => <span>{record?.Event?.name || "-"}</span>}
     />
     <TextField source="formNo" />
     <TextField source="HOFId" label="HOF ID" />
@@ -16,6 +16,11 @@ export default () => (
       label="Members"
       source="familyMembers"
       render={(record) => <span>{record?.familyMembers?.length}</span>}
+    />
+    <BooleanField
+      label="Late Payment"
+      source="latePayment"
+      render={(record) => <span>{record?.latePayment ? "Yes" : "No"}</span>}
     />
     <FunctionField
       label="Submitter"
